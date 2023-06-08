69f285e98ed863c57c833f4beb31d699b4c808c3
Flávio Arêas <vflavio9@gmail.com>
2023-06-08 19:17:53 -0300

    refactor: ajustando prisma com novos atributos student

--- src/main/adapter/prisma-entity.ts
+++ src/main/adapter/prisma-entity.ts
@@ -2,7 +2,43 @@ import { Student } from '@domain/entities/student';
 import { Student as PrismaStudent } from '@prisma/client';
 
 export const prismaAdapterStudent = (prismaStudent: PrismaStudent): Student => {
-  const { email, id, nome } = prismaStudent;
-  const student = Student.create({ email, nome }, id);
+  const {
+    email,
+    id,
+    nome,
+    CPF,
+    RG,
+    emailResponsavel,
+    endereco,
+    matricula,
+    nascimento,
+    nomeMae,
+    nomePai,
+    serie,
+    sexo,
+    status,
+    telefoneMae,
+    telefonePai,
+  } = prismaStudent;
+  const student = Student.create(
+    {
+      email,
+      nome,
+      CPF,
+      emailResponsavel,
+      endereco,
+      matricula,
+      nascimento,
+      nomeMae,
+      nomePai,
+      RG,
+      serie,
+      sexo,
+      status,
+      telefoneMae,
+      telefonePai,
+    },
+    id
+  );
   return student;
 };
