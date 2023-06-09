0903d0c46bfbe1a6b00b2687ce21e620657453db
Flávio Arêas <vflavio9@gmail.com>
2023-06-09 19:10:52 -0300

    feat: garantindo que o UpdateEmployeeController é chamado corretamente

---
+++ src/application/controllers/employees/update-employee.spec.ts
@@ -0,0 +1,106 @@
+import { describe, expect, test, vi } from 'vitest';
+
+import { MissingParamError, ServerError } from '@application/errors';
+import {
+  EmployeeRepository,
+  UpdateEmployeeData,
+} from '@application/repositories/employee';
+import { InMemoryEmployeeRepository } from '@application/repositories/in-memory/in-memory-employees';
+import { UpdateEmployee } from '@domain/use-cases/employee';
+import { UpdateEmployeeController } from './update-employee';
+
+const makeUpdateEmployee = (
+  employeeRepository: EmployeeRepository
+): UpdateEmployee => {
+  class UpdateEmployeeStub implements UpdateEmployee {
+    constructor(private employeeRepository: EmployeeRepository) {}
+    async execute(id: string, data: UpdateEmployeeData): Promise<void> {
+      return await Promise.resolve();
+    }
+  }
+  return new UpdateEmployeeStub(employeeRepository);
+};
+
+interface SutTypes {
+  sut: UpdateEmployeeController;
+  updateEmployee: UpdateEmployee;
+}
+
+const makeSut = (): SutTypes => {
+  const employeeRepository = new InMemoryEmployeeRepository();
+  const updateEmployee = makeUpdateEmployee(employeeRepository);
+  const sut = new UpdateEmployeeController(updateEmployee);
+  return {
+    sut,
+    updateEmployee,
+  };
+};
+
+describe('Update Employee Controller', () => {
+  test('should return 400 if no id is provided', async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      params: {},
+      body: {
+        nome: 'nome',
+        email: 'email@example.com',
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('id'));
+  });
+
+  test('should call UpdateEmployee with correct values', async () => {
+    const { sut, updateEmployee } = makeSut();
+    const updateEmployeeSpy = vi.spyOn(updateEmployee, 'execute');
+    const httpRequest = {
+      params: {
+        id: 'id',
+      },
+      body: {
+        nome: 'nome',
+        email: 'email@example.com',
+      },
+    };
+    await sut.handle(httpRequest);
+    expect(updateEmployeeSpy).toHaveBeenCalledWith('id', {
+      nome: 'nome',
+      email: 'email@example.com',
+    });
+  });
+
+  test('should return 500 if UpdateEmployee throws', async () => {
+    const { sut, updateEmployee } = makeSut();
+    vi.spyOn(updateEmployee, 'execute').mockImplementationOnce(async () => {
+      return await Promise.reject(new Error(''));
+    });
+    const httpRequest = {
+      params: {
+        id: 'id',
+      },
+      body: {
+        nome: 'nome',
+        email: 'email@example.com',
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
+  });
+
+  test('should return 200 on success', async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      params: {
+        id: 'id',
+      },
+      body: {
+        nome: 'nome',
+        email: 'email@example.com',
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+  });
+});
---
+++ src/application/controllers/employees/update-employee.ts
@@ -0,0 +1,23 @@
+import { MissingParamError } from '@application/errors';
+import { badRequest, ok, serverError } from '@application/helpers/http-helper';
+import { Controller } from '@core/application/controller';
+import { HttpRequest, HttpResponse } from '@core/helpers/http';
+import { UpdateEmployee } from '@domain/use-cases/employee';
+
+export class UpdateEmployeeController implements Controller {
+  constructor(private readonly updateEmployee: UpdateEmployee) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { id } = httpRequest.params;
+      const data = httpRequest.body;
+      if (!id) {
+        return badRequest(new MissingParamError('id'));
+      }
+      await this.updateEmployee.execute(id, data);
+      return ok();
+    } catch (err) {
+      return serverError();
+    }
+  }
+}
