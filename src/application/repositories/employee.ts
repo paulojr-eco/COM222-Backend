bb7fa25ddcf8d246b8e6c43e735264287242c1a3
Flávio Arêas <vflavio9@gmail.com>
2023-06-09 17:55:14 -0300

    feat: criando employee repository

---
+++ src/application/repositories/employee.ts
@@ -0,0 +1,13 @@
+import { Employee, IEmployee } from '@domain/entities/employee';
+
+export type CreateEmployeeData = IEmployee;
+
+export type UpdateEmployeeData = Partial<IEmployee>;
+
+export interface EmployeeRepository {
+  create(data: CreateEmployeeData): Promise<void>;
+  get(): Promise<Employee[]>;
+  getById(id: string): Promise<Employee | null>;
+  update(id: string, data: UpdateEmployeeData): Promise<void>;
+  delete(id: string): Promise<void>;
+}
---
+++ src/application/repositories/in-memory/in-memory-employees.ts
@@ -0,0 +1,44 @@
+import {
+  CreateEmployeeData,
+  EmployeeRepository,
+  UpdateEmployeeData,
+} from '@application/repositories/employee';
+import { Employee } from '@domain/entities/employee';
+
+export class InMemoryEmployeeRepository implements EmployeeRepository {
+  public employees: Employee[] = [];
+
+  async create(employee: CreateEmployeeData, id?: string): Promise<void> {
+    this.employees.push(
+      Employee.create(
+        {
+          ...employee,
+        },
+        id
+      )
+    );
+  }
+
+  async get(): Promise<Employee[]> {
+    return this.employees;
+  }
+
+  async getById(id: string): Promise<Employee | null> {
+    return this.employees.find((employee) => employee.id === id) ?? null;
+  }
+
+  async update(id: string, data: UpdateEmployeeData): Promise<void> {
+    this.employees.forEach((employee) => {
+      if (employee.id === id) {
+        employee.props = {
+          ...employee.props,
+          ...data,
+        };
+      }
+    });
+  }
+
+  async delete(id: string): Promise<void> {
+    this.employees = this.employees.filter((employee) => employee.id !== id);
+  }
+}
