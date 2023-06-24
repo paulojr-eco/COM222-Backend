a85c475accea2e156e1c29edd796353587196ec7
Flávio Arêas <vflavio9@gmail.com>
2023-06-24 11:32:21 -0300

    feat: criando assinaturas para account

---
+++ src/domain/entities/account.ts
@@ -0,0 +1,17 @@
+import { Entity } from '@core/domain/entity';
+
+export interface IAccount {
+  email: string;
+  password: string;
+}
+
+export class Account extends Entity<IAccount> {
+  private constructor(props: IAccount, id?: string) {
+    super(props, id);
+  }
+
+  static create(props: IAccount, id?: string) {
+    const account = new Account(props, id);
+    return account;
+  }
+}
