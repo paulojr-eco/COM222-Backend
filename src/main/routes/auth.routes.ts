572f2029ed1118f06f4d8fcbb5c51d29022656a8
Flávio Arêas <vflavio9@gmail.com>
2023-06-25 13:45:46 -0300

    feat: garantindo que as rotas Auth funcione corretamente

--- src/main/routes/auth.routes.ts
+++ src/main/routes/auth.routes.ts
@@ -1,8 +1,12 @@
 import { type Router } from 'express';
 
 import { expressAdapterRoute } from '@main/adapter/express-route';
-import { makeSignUpController } from '@main/factories/auth';
+import {
+  makeSignInController,
+  makeSignUpController,
+} from '@main/factories/auth';
 
 export default (router: Router): void => {
   router.post('/sign-up', expressAdapterRoute(makeSignUpController()));
+  router.post('/sign-in', expressAdapterRoute(makeSignInController()));
 };
