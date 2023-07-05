fcb7ba870cff768bf3dea776276d5636d1d8463d
Flávio Arêas <vflavio9@gmail.com>
2023-07-05 19:47:45 -0300

    refactor: aplicando middleware is nas rotas de employees

--- src/main/routes/employees.routes.ts
+++ src/main/routes/employees.routes.ts
@@ -8,26 +8,32 @@ import {
   makeGetEmployeesController,
   makeUpdateEmployeeController,
 } from '@main/factories/employee';
+import { is } from '@main/middlewares';
 
 export default (router: Router): void => {
   router.post(
     '/funcionarios',
+    is(['ADMIN']),
     expressAdapterRoute(makeCreateEmployeeController())
   );
   router.get(
     '/funcionarios',
+    is(['ADMIN', 'USER']),
     expressAdapterRoute(makeGetEmployeesController())
   );
   router.get(
     '/funcionarios/:id',
+    is(['ADMIN', 'USER']),
     expressAdapterRoute(makeGetEmployeeByIdController())
   );
   router.put(
     '/funcionarios/:id',
+    is(['ADMIN']),
     expressAdapterRoute(makeUpdateEmployeeController())
   );
   router.delete(
     '/funcionarios/:id',
+    is(['ADMIN']),
     expressAdapterRoute(makeDeleteEmployeeController())
   );
 };
